e4cec1045944dfe56f0cba99c33b325ff801f8f7
Luo Yumo <luoyumo@cubbli18-gold.local>
2020-12-10 14:55:47 +0200

    eureka as server, ocnfigure maven parent project

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -1,13 +1,13 @@
-package com.course.imooc;
+package com.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImoocApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImoocApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
