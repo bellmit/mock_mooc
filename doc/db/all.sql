c9746caf794b470fa772883461a8b5d96dbe0c8d
Luo Yumo <luoyumo@cubbli18-gold.local>
2020-12-23 11:25:38 +0200

    add chapter scheme and its mapper

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,9 +1,24 @@
-drop table if exists `test`;
-create table `test` (
- `id` char(8) not null default '' comment 'id',
- `name` varchar(50) comment 'name',
- primary key (`id`)
-) engine=innodb default charset=utf8mb4;
+drop table if exists `chapter`;
+create table `chapter`
+(
+    `id`        char(8) not null,
+    `course_id` char(8),
+    `name`      varchar(50),
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4;
 
-insert into `test` (`id`, `name`) values(1, 'tester');
-insert into `test` (`id`, `name`) values(2, 'tester2');
+#test
+drop table if exists `test`;
+create table `test`
+(
+    `id`   char(8) not null default '' comment 'id',
+    `name` varchar(50) comment 'name',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4;
+
+insert into `test` (`id`, `name`)
+values (1, 'tester');
+insert into `test` (`id`, `name`)
+values (2, 'tester2');
