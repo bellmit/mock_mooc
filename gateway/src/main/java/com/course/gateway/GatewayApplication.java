2d3f70300854a00823ac08e76ea5eea1353c6206
Luo Yumo <luoyumo@cubbli18-gold.local>
2020-12-24 19:14:35 +0200

    move cors config to gateway

--- gateway/src/main/java/com/course/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -5,7 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
+
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -20,4 +26,22 @@ public class GatewayApplication {
         LOG.info("System address: \thttp:/127.0.0.1:{}", env.getProperty("server.port"));
     }
 
+    // cors config
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(Boolean.TRUE);
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.addAllowedOrigin("*");
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+
+    }
+
 }
+
