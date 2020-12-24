6da1ff07a7bb35af43eb4de9af62529d511a790d
Luo Yumo <luoyumo@cubbli18-gold.local>
2020-12-24 18:58:36 +0200

    frontend only visits port 9000

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -89,7 +89,7 @@ export default {
   methods: {
     list() {
       let _this = this;
-      _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then((response)=>{
+      _this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/list').then((response)=>{
         console.log('chapter result: ', response);
         _this.chapters = response.data;
       })
