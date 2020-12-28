a3f10410e2fa11eb953ec995c9b765735d113ecd
Luo Yumo <luoyumo@cubbli18-gold.local>
2020-12-28 15:43:18 +0200

    divide into multiple pages

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -89,9 +89,13 @@ export default {
   methods: {
     list() {
       let _this = this;
-      _this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/list').then((response)=>{
+      _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list', {
+        page: 1,
+        size: 1
+      })
+          .then((response)=>{
         console.log('chapter result: ', response);
-        _this.chapters = response.data;
+        _this.chapters = response.data.list;
       })
     }
   }
