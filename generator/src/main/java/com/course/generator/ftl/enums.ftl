f534290698afa92f85afc8bddd3228a824a3d260
luoyumo <yumo.luo@hotmail.com>
2021-01-05 17:36:44 +0200

    multiple enum generator

--- generator/src/main/java/com/course/generator/ftl/enums.ftl
+++ generator/src/main/java/com/course/generator/ftl/enums.ftl
@@ -1,2 +1,7 @@
-${objectName} = {<#list objects as object>${object.name()}: {key: "${object.code}", value: "${object.desc}"}, </#list>}
-<#--${objectName} = {<#list objects as object>${object.name()}: },</#list>}-->
+<#--${objectName} = {<#list objectList as object><#list object as enum>${enum.name()}: {key: "${enum.code}", value: "${enum.desc}"}, </#list></#list>}-->
+
+<#list objectList as object>
+<#list object as key, value>
+${key}={<#list value as enum>${enum.name()}: {key: "${enum.code}", value: "${enum.desc}"}, </#list>}
+</#list>
+</#list>
--- generator/src/main/java/com/course/generator/vue/EnumGenerator.java
+++ generator/src/main/java/com/course/generator/vue/EnumGenerator.java
@@ -3,22 +3,61 @@ package com.course.generator.vue;
 import com.course.generator.util.FreemarkerUtil;
 import com.course.server.enums.SectionChargeEnum;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EnumGenerator {
 
     static String toEnumPath = "/home/luoyumo/Desktop/mooc_frontend/admin/public/static/js/enums.js";
+    static List<Map<String,Object[]>> objectList = new ArrayList<>();
 
     public static void main(String[] args) throws Exception{
-        String objectName = "SECTION_CHARGE";
-        Object[] objects = SectionChargeEnum.class.getEnumConstants();
-
+        insert(SectionChargeEnum.class);
         Map<String, Object> map = new HashMap<>();
-        map.put("objectName", objectName);
-        map.put("objects", objects);
+        map.put("objectList", objectList);
 
         FreemarkerUtil.initConfig("enums.ftl");
         FreemarkerUtil.generator(toEnumPath, map);
     }
+
+    private static void insert(Class clazz){
+        Map<String, Object[]> map = new HashMap<>();
+        map.put(toUnderLine(clazz.getSimpleName()), clazz.getEnumConstants());
+        objectList.add(map);
+    }
+
+    /**
+     * _section_charge_enum => SECTION_CHARGE
+     * @param str
+     * @return
+     */
+    private static String toUnderLine(String str){
+        String result = underline(str).toString();
+        return result.substring(1, result.length()).toUpperCase().replace("_ENUM", "");
+    }
+
+    /**
+     * SectionChargeEnum => _section_charge_enum
+     * @param str
+     * @return
+     */
+    private static StringBuffer underline(String str){
+        Pattern pattern = Pattern.compile("[A-Z]");
+        Matcher matcher = pattern.matcher(str);
+        StringBuffer sb = new StringBuffer(str);
+        if(matcher.find()){
+            sb = new StringBuffer();
+            matcher.appendReplacement(sb, "_"+matcher.group(0).toLowerCase());
+            matcher.appendTail(sb);
+        } else {
+            return sb;
+        }
+        return underline(sb.toString());
+    }
+
+
 }
