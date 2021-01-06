4a42eca370d62b3646446aab95a9806990b5ebc8
luoyumo <yumo.luo@hotmail.com>
2021-01-06 22:10:09 +0200

    basic code of category

---
+++ business/src/main/java/com/course/business/controller/admin/CategoryController.java
@@ -0,0 +1,48 @@
+package com.course.business.controller.admin;
+
+import com.course.server.dto.CategoryDto;
+import com.course.server.dto.PageDto;
+import com.course.server.dto.ResponseDto;
+import com.course.server.service.CategoryService;
+import com.course.server.util.ValidatorUtil;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/admin/category")
+public class CategoryController {
+
+    public static final String BUSINESS_NAME = "category";
+
+    @Resource
+    private CategoryService categoryService;
+
+    @PostMapping("/list")
+    public ResponseDto list(@RequestBody PageDto pageDto) {
+        ResponseDto responseDto = new ResponseDto();
+        categoryService.list(pageDto);
+        responseDto.setContent(pageDto);
+        return responseDto;
+    }
+
+    @PostMapping("/save")
+    public ResponseDto save(@RequestBody CategoryDto categoryDto) {
+        //validation
+        ValidatorUtil.require(categoryDto.getParent(), "parent");
+        ValidatorUtil.require(categoryDto.getName(), "name");
+        ValidatorUtil.length(categoryDto.getName(), "name", 1, 50);
+
+        ResponseDto responseDto = new ResponseDto();
+        categoryService.save(categoryDto);
+        responseDto.setContent(categoryDto);
+        return responseDto;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseDto delete(@PathVariable String id) {
+        ResponseDto responseDto = new ResponseDto();
+        categoryService.delete(id);
+        return responseDto;
+    }
+}
--- doc/db/all.sql
+++ doc/db/all.sql
@@ -77,6 +77,60 @@ create table `section`
 insert into `section` (id, title, course_id, chapter_id, video, time, charge, sort, created_at, updated_at)
 values ('00000001', 'section01', '00000001', '00000001', '', 500, 'F', 1, now(), now());
 
+-- category
+drop table if exists `category`;
+create table `category` (
+                            `id` char(8) not null default '' comment 'id',
+                            `parent` char(8) not null default '' comment 'parent id',
+                            `name` varchar(50) not null,
+                            `sort` int,
+                            primary key (`id`)
+) engine=innodb default charset=utf8mb4;
+
+insert into `category` (id, parent, name, sort) values ('00000100', '00000000', 'frontend', 100);
+insert into `category` (id, parent, name, sort) values ('00000101', '00000100', 'html/css', 101);
+insert into `category` (id, parent, name, sort) values ('00000102', '00000100', 'javascript', 102);
+insert into `category` (id, parent, name, sort) values ('00000103', '00000100', 'vue.js', 103);
+insert into `category` (id, parent, name, sort) values ('00000104', '00000100', 'react.js', 104);
+insert into `category` (id, parent, name, sort) values ('00000105', '00000100', 'angular', 105);
+insert into `category` (id, parent, name, sort) values ('00000106', '00000100', 'node.js', 106);
+insert into `category` (id, parent, name, sort) values ('00000107', '00000100', 'jquery', 107);
+insert into `category` (id, parent, name, sort) values ('00000108', '00000100', 'applet', 108);
+insert into `category` (id, parent, name, sort) values ('00000200', '00000000', 'backend', 200);
+insert into `category` (id, parent, name, sort) values ('00000201', '00000200', 'java', 201);
+insert into `category` (id, parent, name, sort) values ('00000202', '00000200', 'springboot', 202);
+insert into `category` (id, parent, name, sort) values ('00000203', '00000200', 'spring cloud', 203);
+insert into `category` (id, parent, name, sort) values ('00000204', '00000200', 'ssm', 204);
+insert into `category` (id, parent, name, sort) values ('00000205', '00000200', 'python', 205);
+insert into `category` (id, parent, name, sort) values ('00000206', '00000200', 'crawler', 206);
+insert into `category` (id, parent, name, sort) values ('00000300', '00000000', 'mobile development', 300);
+insert into `category` (id, parent, name, sort) values ('00000301', '00000300', 'android', 301);
+insert into `category` (id, parent, name, sort) values ('00000302', '00000300', 'ios', 302);
+insert into `category` (id, parent, name, sort) values ('00000303', '00000300', 'react native', 303);
+insert into `category` (id, parent, name, sort) values ('00000304', '00000300', 'ionic', 304);
+insert into `category` (id, parent, name, sort) values ('00000400', '00000000', 'advanced technologies', 400);
+insert into `category` (id, parent, name, sort) values ('00000401', '00000400', 'microservices', 401);
+insert into `category` (id, parent, name, sort) values ('00000402', '00000400', 'block chain', 402);
+insert into `category` (id, parent, name, sort) values ('00000403', '00000400', 'machine learning', 403);
+insert into `category` (id, parent, name, sort) values ('00000404', '00000400', 'deep learning', 404);
+insert into `category` (id, parent, name, sort) values ('00000405', '00000400', 'data mining', 405);
+insert into `category` (id, parent, name, sort) values ('00000500', '00000000', 'cloud computing', 500);
+insert into `category` (id, parent, name, sort) values ('00000501', '00000500', 'big data', 501);
+insert into `category` (id, parent, name, sort) values ('00000502', '00000500', 'hadoop', 502);
+insert into `category` (id, parent, name, sort) values ('00000503', '00000500', 'spark', 503);
+insert into `category` (id, parent, name, sort) values ('00000504', '00000500', 'hbase', 504);
+insert into `category` (id, parent, name, sort) values ('00000505', '00000500', 'Ali cloud', 505);
+insert into `category` (id, parent, name, sort) values ('00000506', '00000500', 'docker', 506);
+insert into `category` (id, parent, name, sort) values ('00000507', '00000500', 'kubernetes', 507);
+insert into `category` (id, parent, name, sort) values ('00000600', '00000000', 'maintenance', 600);
+insert into `category` (id, parent, name, sort) values ('00000604', '00000600', 'linux', 604);
+insert into `category` (id, parent, name, sort) values ('00000605', '00000600', 'testing', 605);;
+insert into `category` (id, parent, name, sort) values ('00000700', '00000000', 'database', 700);
+insert into `category` (id, parent, name, sort) values ('00000701', '00000700', 'mysql', 701);
+insert into `category` (id, parent, name, sort) values ('00000702', '00000700', 'redis', 702);
+insert into `category` (id, parent, name, sort) values ('00000703', '00000700', 'mongodb', 703);
+
+
 #test
 drop table if exists `test`;
 create table `test`
